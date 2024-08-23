3784fec2c7174b15c4c92d2e903a97116cf0cee6
mcenerym <mike.mcenery1@gmail.com>
2024-08-23 19:15:39 -0400

    set scale on profile image

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,7 @@ export default function App() {
 				<div className='flex place-items-center'>
 					<div className='flex'>
 						<div className='w-80 h-80 relative overflow-hidden rounded-full'>
-							<img
-								className='scale-110 ml-[-12px] mt-2'
-								src={profile}
-								alt='profile'
-							/>
+							<img src={profile} alt='profile' />
 						</div>
 						<div className='ml-8 text-white'>
 							<label className='text-5xl'>Mike McEnery</label>
