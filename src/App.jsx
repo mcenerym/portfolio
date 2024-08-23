e58b14cffd1f68732e3832d37f042ae17d42e388
mcenerym <mike.mcenery1@gmail.com>
2024-08-23 18:44:44 -0400

    Completed basic site implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import profile from './assets/profile.jpg'
+export default function App() {
+	return (
+		<div>
+			<div className='h-12 bg-[#366889]'>
+				<div className='pt-3 pr-2 text-right'>
+					<a
+						href='#'
+						className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+						About
+					</a>
+					<a
+						href='#'
+						className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+						Blog
+					</a>
+					<a
+						href='#'
+						className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+						Projects
+					</a>
+				</div>
+			</div>
+			<div className='flex justify-center h-96 bg-[#153148]'>
+				<div className='flex place-items-center'>
+					<div className='flex'>
+						<div className='w-80 h-80 relative overflow-hidden rounded-full'>
+							<img className='max-w-80' src={profile} />
+						</div>
+						<div className='ml-8 text-white'>
+							<label className='text-5xl'>Mike McEnery</label>
+							<br />
+							<label className='italic'>Quality Software Delivered</label>
+							<br />
+							<div className='mt-8 italic w-[600px] text-white'>
+								Senior software engineer with 16 years of experience in full
+								stack development with a history of delivering high quality
+								applications through great teamwork and communication. Expertise
+								includes improving system architecture, improving team
+								effectiveness and exceeding users expectations.
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
-
-export default App
