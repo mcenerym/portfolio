5a1c6fca659659e4717a113ce548df9a16482574
mcenerym <mike.mcenery1@gmail.com>
2024-08-23 19:16:48 -0400

    Unreverting profile pics changes

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,11 @@ export default function App() {
 				<div className='flex place-items-center'>
 					<div className='flex'>
 						<div className='w-80 h-80 relative overflow-hidden rounded-full'>
-							<img src={profile} alt='profile' />
+							<img
+								className='scale-110 ml-[-12px] mt-2'
+								src={profile}
+								alt='profile'
+							/>
 						</div>
 						<div className='ml-8 text-white'>
 							<label className='text-5xl'>Mike McEnery</label>
